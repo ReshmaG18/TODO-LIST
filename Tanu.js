bf8c7d80be1c8dd70afe895b16a6d6763053f474
ReshmaG18 <147267784+ReshmaG18@users.noreply.github.com>
2023-11-04 20:31:18 +0530

    Add files via upload

---
+++ Tanu.js
@@ -0,0 +1,21 @@
+// const { relative } = require("path");
+
+let input= document.getElementById("inp");
+let text = document.querySelector(".text");
+function Add(){
+    if(input.value == ""){
+        alert("Please Enter Task")
+    }else{
+        let newEle = document.createElement("ul");
+        newEle.innerHTML=`${input.value}<i class="fa-solid fa-trash"></i>`;
+        text.appendChild(newEle);
+        input.value ="";
+        newEle.querySelector("i").addEventListener("click",remove);
+        function remove (){
+            newEle.remove()
+        }
+    }
+    
+}
+
+
